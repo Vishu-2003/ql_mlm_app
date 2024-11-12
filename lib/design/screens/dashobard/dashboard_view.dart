30dad7a7c3e7c33221ff45c3e812c9ab63d4933a
Vishu-2003 <vishu@nesscale.com>
2024-11-12 17:44:42 +0530

    git practise

--- lib/design/screens/dashobard/dashboard_view.dart
+++ lib/design/screens/dashobard/dashboard_view.dart
@@ -158,7 +158,6 @@ class DashboardView extends StatelessWidget {
                     activeIcon: selectIcon(AppIcons.home, color: getPrimaryColor),
                   ),
                   BottomNavigationBarItem(
-                    icon: selectIcon(AppIcons.wallet),
                     label: TranslationController.td.wallet,
                     activeIcon: selectIcon(AppIcons.wallet, color: getPrimaryColor),
                   ),
